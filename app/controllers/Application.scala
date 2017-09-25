78b9f0d1d2553c4e44acc8487068bf5789f9dc55
clarkdm <dmclark@hotmail.co.uk>
2017-09-25 16:51:43 +0100

    Link the database to the project

--- app/controllers/Application.scala
+++ app/controllers/Application.scala
@@ -1,9 +1,13 @@
 package controllers
 
-import play.api._
+import javax.inject.Inject
+import play.api.i18n.{I18nSupport, MessagesApi}
 import play.api.mvc._
+import play.modules.reactivemongo.{MongoController, ReactiveMongoApi, ReactiveMongoComponents}
 
-class Application extends Controller {
+class Application @Inject()(val messagesApi: MessagesApi, val reactiveMongoApi: ReactiveMongoApi)
+    extends Controller with I18nSupport with MongoController with ReactiveMongoComponents {
+
 
   def index = Action {
     Ok(views.html.index("Your new application is ready."))
