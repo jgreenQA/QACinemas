327fb98b3f967467f6c6b2d992856698aaa59972
clarkdm <dmclark@hotmail.co.uk>
2017-09-26 12:00:40 +0100

    Fix Link the new database to the project

---
+++ app/models/DBs.scala
@@ -0,0 +1,24 @@
+package models
+
+import play.api.data.Forms._
+import play.api.data._
+
+
+
+
+
+object Formats {
+
+  import play.api.libs.json.Json
+
+  // Generates Writes and Reads for Feed and User thanks to Json Macros
+
+
+
+
+
+
+}
+
+
+
